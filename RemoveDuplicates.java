9c4f1995829c3707240733edbef05e09730ee7e8
techelix2025 <helixfrancisco92@gmail.com>
2025-11-03 17:31:40 +0800

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 
 public class RemoveDuplicates {
 
-    // Custom function to remove duplicate characters
     public static String removeDuplicates(String str) {
         String result = "";
 
         for (int i = 0; i < str.length(); i++) {
             char currentChar = str.charAt(i);
 
-            // Add the character only if it's not already in result
             if (result.indexOf(currentChar) == -1) {
                 result += currentChar;
             }
@@ -30,3 +28,4 @@ public class RemoveDuplicates {
         scanner.close();
     }
 }
+
